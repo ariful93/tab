a6a7b8eb23ca4e392d347f9d0847e2c983e280a6
ariful93 <arif13414@gmail.com>
2025-01-11 16:22:18 +0600

    Added tab animation

--- js/app.js
+++ js/app.js
@@ -1,11 +1,19 @@
 let tabLists = document.querySelectorAll('.tab-items')
+let indicator = document.querySelector('.indicator')
 
 
 tabLists.forEach((list) => {
     let tabItems = list.querySelectorAll('.tab-btn')
     let tabContents = list.querySelectorAll('.tab-content')
-    tabItems.forEach((item) => {
+    tabItems.forEach((item, index) => {
+        let widthPercent = 100 / tabItems.length;
+        indicator.style.width = `${widthPercent}%`;
+        
         item.addEventListener('click', () => {
+            let width = indicator.offsetWidth
+            indicator.style.transform = `translate(calc(${width}px * ${index}))`;
+            
+            
             // remove active class
             tabItems.forEach((item) => {
                 item.classList.remove('active')
